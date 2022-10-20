e5383f29218b62fdb1e7ffcc01aea836893d3a38
mtrentz <mateus.trentz@gmail.com>
2022-10-20 00:18:41 -0300

    prog

---
+++ minesweeper/sketch/game.ts
@@ -0,0 +1,111 @@
+interface Cell {
+    // Top left corner
+    p1: p5.Vector;
+    // Bottom right corner
+    p2: p5.Vector;
+    isBomb: boolean;
+    isFlagged: boolean;
+    isRevealed: boolean;
+    // Number of bombs in adjacent cells
+    nearbyBombs: number;
+}
+
+class Game {
+    rows: number;
+    cols: number;
+
+    // 2D array of cells
+    cells: Cell[][];
+
+    // Number of bombs
+    bombs: number;
+
+    constructor(rows: number = 20, cols: number = 20, bombs: number = 40) {
+        this.rows = rows;
+        this.cols = cols;
+        this.bombs = bombs;
+        this.cells = [];
+    }
+
+    setup() {
+        // Figure out the size of each cell based on the canvas size
+        const cellWidth = (width - 10) / this.cols;
+        const cellHeight = (height - 10) / this.rows;
+
+        // Made up origin point, so that the board doesnt
+        // start at (0,0)
+        const origin = createVector(5, 5);
+
+        // Create the cells
+        for (let i = 0; i < this.rows; i++) {
+            this.cells[i] = [];
+            for (let j = 0; j < this.cols; j++) {
+                const p1 = createVector(
+                    origin.x + i * cellWidth,
+                    origin.y + j * cellHeight
+                );
+                const p2 = createVector(
+                    origin.x + (i + 1) * cellWidth,
+                    origin.y + (j + 1) * cellHeight
+                );
+                this.cells[i][j] = {
+                    p1,
+                    p2,
+                    isBomb: false,
+                    isFlagged: false,
+                    isRevealed: false,
+                    nearbyBombs: 0,
+                };
+            }
+        }
+
+        let placedBombs = 0;
+
+        // Pick random cells until we have placed all the bombs
+        // First check if there are enough cells, else
+        // throw error
+        if (this.rows * this.cols < this.bombs) {
+            throw new Error("Too many bombs");
+        }
+
+        while (placedBombs < this.bombs) {
+            const i = floor(random(this.rows));
+            const j = floor(random(this.cols));
+            const cell = this.cells[i][j];
+            if (!cell.isBomb) {
+                cell.isBomb = true;
+                placedBombs++;
+            }
+        }
+    }
+
+
+    // count() {
+    //     // Iterate 
+    // }
+
+
+    draw() {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                const cell = this.cells[i][j];
+                push();
+                stroke(0);
+                // noFill();
+                if (cell.isRevealed) {
+                    // Green
+                    fill(0, 255, 0);
+                } else if (cell.isFlagged) {
+                    // Fill light blue
+                    fill(0, 0, 255, 100);
+                } else if (cell.isBomb) {
+                    // Fill red
+                    fill(255, 0, 0);
+                }
+
+                rect(cell.p1.x, cell.p1.y, cell.p2.x - cell.p1.x, cell.p2.y - cell.p1.y);
+                pop();
+            }
+        }
+    }
+}
---
+++ minesweeper/sketch/sketch.ts
@@ -0,0 +1,39 @@
+let game: Game;
+
+function setup() {
+  createCanvas(400, 400);
+  game = new Game();
+  game.setup();
+}
+
+function draw() {
+  background(150);
+  game.draw();
+  // console.log(game.cells)
+  // noLoop()
+}
+
+// Watch for mouse clicks
+function mousePressed() {
+  // Iterate over the cells in the game
+  for (let i = 0; i < game.rows; i++) {
+    for (let j = 0; j < game.cols; j++) {
+      const cell = game.cells[i][j];
+      // Check if the mouse is inside the cell
+      if (
+        mouseX > cell.p1.x &&
+        mouseX < cell.p2.x &&
+        mouseY > cell.p1.y &&
+        mouseY < cell.p2.y
+      ) {
+        console.log("Clicked on cell", i, j);
+        // Check for left click
+        if (mouseButton === LEFT) {
+          // Reveal the cell
+          cell.isRevealed = true;
+          console.log("Revealed cell", i, j);
+        }
+      }
+    }
+  }
+}
